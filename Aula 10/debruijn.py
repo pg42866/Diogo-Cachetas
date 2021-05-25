bdeef3b9f2a0e116422b787dfc2305992b2f630d
pg42866 <73937757+pg42866@users.noreply.github.com>
2021-05-25 21:48:18 +0100

    Add files via upload

---
+++ Aula 10/debruijn.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+from MyGraph import MyGraph
+
+class DeBruijnGraph (MyGraph):
+    
+    def __init__(self, frags):
+        MyGraph.__init__(self, {})
+        self.create_deBruijn_graph(frags)
+
+    def add_edge(self, o, d): #faz um arco entre o e d
+        if o not in self.graph.keys():
+            self.add_vertex(o)
+        if d not in self.graph.keys():
+            self.add_vertex(d)
+        self.graph[o].append(d)
+
+    def in_degree(self, v): #numero de ligações in em v
+        res = 0
+        for k in self.graph.keys(): 
+            if v in self.graph[k]: 
+                res += self.graph[k].count(v)
+        return res
+
+    def create_deBruijn_graph(self, frags): #cria o grafo
+        for seq in frags:
+            suf = suffix(seq)
+            self.add_vertex(suf)
+            pref = prefix(seq)
+            self.add_vertex(pref)
+            self.add_edge(pref, suf)
+
+    def seq_from_path(self, path):
+        seq = path[0]
+        for i in range(1,len(path)):
+            nxt = path[i]
+            seq += nxt[-1]
+        return seq 
+    
+def suffix (seq): 
+    return seq[1:]
+    
+def prefix(seq):
+    return seq[:-1]
+
+def composition(k, seq):
+    res = []
+    for i in range(len(seq)-k+1):
+        res.append(seq[i:i+k])
+    res.sort()
+    return res
+
+
+
+def test1():
+    frags = [ "ATA", "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA", "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+    dbgr = DeBruijnGraph(frags)
+    dbgr.print_graph()
+    
+    
+def test2():
+    frags = [ "ATA", "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA", "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+    dbgr = DeBruijnGraph(frags)
+    dbgr.print_graph()
+    print (dbgr.check_nearly_balanced_graph())
+    print (dbgr.eulerian_path())
+
+
+def test3():
+    orig_sequence = "ATGCAATGGTCTG"
+    frags = composition(3, orig_sequence)
+    # ... completar
+
+
+
+test1()
+print()
+#test2()
+#print()
+#test3()
+    
---
+++ Aula 10/overlap_graphs.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+from MyGraph import MyGraph
+
+class OverlapGraph(MyGraph):
+    
+    def __init__(self, frags):
+        MyGraph.__init__(self, {})
+        self.create_overlap_graph(frags)
+
+#    def __init__(self, frags, reps = False):
+#        if reps: self.create_overlap_graph_with_reps(frags)
+#        else: self.create_overlap_graph(frags)
+#        self.reps = reps
+        
+    
+    ## create overlap graph from list of sequences (fragments)
+    def create_overlap_graph(self, frags): #cria o grafo a partir da ligação dos sufixos com os prefixos
+        ##add vertices
+        for seq in frags: #adicionar todos os fragmentos como vertices do grafo
+            self.add_vertex(seq)
+        #add edges
+        for seq in frags: #para cada fragmento
+            suf = suffix(seq) #encontra o sufixo desse fragmento
+            for seq2 in frags: #para cada fragmento
+                if prefix(seq2) == suf: #se o prefixo deste fragmento for igual ao sufixo do fragmento do primeiro ciclo
+                    self.add_edge(seq, seq2) #cria-se um arco entre os dois fragmentos
+        
+    def create_overlap_graph_with_reps(self, frags):  # caso de replicas de fragmentos, ou seja se houverem repetições entre fragmento, é dado um id a cada vertice
+        idnum = 1
+        for seq in frags:
+            self.add_vertex(seq+"-"+str(idnum))
+            idnum += 1
+        idnum = 1
+        for seq in frags:
+            suf = suffix(seq)
+            for seq2 in frags:
+                if prefix(seq2) == suf:
+                    for x in self.get_instances(seq2):
+                        self.add_edge(seq+"-"+str(idnum), x)
+            idnum += 1
+    
+    def get_instances(self, seq): #dá return a todas instancias de uma seq
+        res = []
+        for k in self.graph.keys():
+            if seq in k:
+                res.append(k)
+        return res
+    
+    def get_seq(self, node): #vê se o node existe, caso exista e se ele se reptir dá return do número
+        if node not in self.graph.keys():
+            return None
+        if self.reps:
+            return node.split("-")[0]
+        else:
+            return node
+    
+    def seq_from_path(self, path): #reconstroi a seq a partir do caminho hamiltoniano
+        if not self.check_if_hamiltonian_path(path): #se não houver um caminho hamiltoniano
+            return None
+        seq = self.get_seq(path[0])
+        for i in range(1, len(path)):
+            nxt = self.get_seq(path[i])
+            seq += nxt[-1]
+        return seq    
+   
+                    
+# auxiliary
+def composition(k, seq):
+    res = []
+    for i in range(len(seq)-k+1):
+        res.append(seq[i:i+k])
+    res.sort()
+    return res
+    
+def suffix (seq): 
+    return seq[1:]
+    
+def prefix(seq):
+    return seq[:-1]
+
+  
+# testing / mains
+def test1():
+    seq = "CAATCATGATG"
+    k = 3
+    print (composition(k, seq))
+   
+def test2():
+    frags = ["ACC", "ATA", "CAT", "CCA", "TAA"]
+    ovgr = OverlapGraph(frags, False)
+    ovgr.print_graph()
+
+def test3():
+     frags = [ "ATA", "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA", "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+     ovgr = OverlapGraph(frags, True)
+     ovgr.print_graph()
+
+def test4():
+    frags = ["ATA",  "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA" , "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+    ovgr = OverlapGraph(frags, True)
+    path = ['ACC−2', 'CCA−8', 'CAT−5', 'ATG−3']
+    print (ovgr.check_if_valid_path(path))
+    print (ovgr.check_if_hamiltonian_path(path))
+    path2 = ['ACC−2', 'CCA−8', 'CAT−5', 'ATG−3', 'TGG−13', 'GGC−10', 'GCA−9', 'CAT−6', 'ATT−4', 'TTT−15', 'TTC−14', 'TCA−12', 'CAT−7', 'ATA−1', 'TAA−11']
+    print (ovgr.check_if_valid_path(path2))
+    print (ovgr.check_if_hamiltonian_path(path2))
+    print (ovgr.seq_from_path(path2))
+
+def test5():
+    frags = [ "ATA", "ACC", "ATG", "ATT", "CAT", "CAT", "CAT", "CCA", "GCA", "GGC", "TAA", "TCA", "TGG", "TTC", "TTT"]
+    ovgr = OverlapGraph(frags, True)
+
+    path = ovgr.search_hamiltonian_path()
+    print(path)
+    print (ovgr.check_if_hamiltonian_path(path))
+    print (ovgr.seq_from_path(path))
+
+def test6():
+    orig_sequence = "CAATCATGATGATGATC"
+    frags = composition(3, orig_sequence)
+    print (frags)
+    ovgr = OverlapGraph(frags, True)
+    ovgr.print_graph()
+    path = ovgr.search_hamiltonian_path()
+    print (path)
+    print (ovgr.seq_from_path(path))
+   
+test1()
+print()
+test2()
+print()
+#test3()
+#print()
+#test4()
+#print()
+#test5()
+#print()
+#test6()
